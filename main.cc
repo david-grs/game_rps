4821415f9b5c2006ccf82cd791c63bf2cb07a73b
David G <david.a.grs@gmail.com>
2016-11-01 20:25:21 +0100

    adding play_again

--- main.cc
+++ main.cc
@@ -1,7 +1,30 @@
 #include <iostream>
 
+namespace io
+{
+bool play_again()
+{
+    char res{' '};
+    while (res != 'y' && res != 'n')
+    {
+        std::cout << "play again? [y/n] ";
+        std::cin >> res;
+        res = std::tolower(res);
+    }
+
+    return res == 'y';
+}
+}
+
 int main()
 {
+    for (bool play = true; play; )
+    {
+
+
+        play = io::play_again();
+    }
+
     return 0;
 }
 
