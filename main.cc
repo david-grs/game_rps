ce72352142ef7937879fcb4628e42ff6844af949
David G <david.a.grs@gmail.com>
2016-11-01 21:16:10 +0100

    adding fight()

--- main.cc
+++ main.cc
@@ -5,6 +5,7 @@
 #include <vector>
 #include <sstream>
 #include <random>
+#include <experimental/optional>
 
 namespace io
 {
@@ -63,6 +64,39 @@ namespace rps
         }
     }
 
+    static symbol read_symbol_stdin()
+    {
+        char s = io::read_one_of("rock, paper, scissors... ?", {'r', 'p', 's'});
+        return to_symbol(s);
+    }
+
+    namespace detail
+    {
+        static std::experimental::optional<std::string> fight(symbol lhs, symbol rhs)
+        {
+            if (lhs == symbol::rock && rhs == symbol::scissors)
+                return {"rock blunts scissors!"};
+            else if (lhs == symbol::paper && rhs == symbol::rock)
+                return {"paper wraps rock!"};
+            else if (lhs == symbol::scissors && rhs == symbol::paper)
+                return {"scissors cuts paper!"};
+            return {};
+        }
+    }
+
+    static std::pair<int, std::string> fight(symbol lhs, symbol rhs)
+    {
+        auto msg = detail::fight(lhs, rhs);
+        if (msg)
+            return {1, *msg};
+
+        msg = detail::fight(rhs, lhs);
+        if (msg)
+            return {-1, *msg};
+
+        return {0, "tie!"};
+    }
+
     struct game
     {
         game()
@@ -76,17 +110,23 @@ namespace rps
 
             static const char choices[] = {'r','p','s'};
             symbol ai_sym = to_symbol(choices[_rng(_gen)]);
+
+            auto p = fight(hum_sym, ai_sym);
+
+            assert(p.first >= -1 && p.first <= 1);
+
+            if (p.first == 1)
+                ++_score_hum;
+            else if (p.first == -1)
+                ++_score_ai;
+
+            std::cout << p.second << std::endl;
         }
 
         int get_scope_ai() const { return _score_ai; }
         int get_scope_hum() const { return _score_hum; }
 
     private:
-        static symbol read_symbol_stdin()
-        {
-            char s = io::read_one_of("rock, paper, scissors... ?", {'r', 'p', 's'});
-            return to_symbol(s);
-        }
 
         int _score_ai = 0;
         int _score_hum = 0;
